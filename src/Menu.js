d839dc1abee96079234d1a356ea8cdc5a9ec32c9
Luciano Braga <lucianobragaweb@gmail.com>
2019-03-01 21:48:15 -0300

    Verificando Plataforma

--- src/Menu.js
+++ src/Menu.js
@@ -4,9 +4,13 @@ import { createDrawerNavigator } from 'react-navigation'
 import Simples from './components/Simples'
 import ParImpar from './components/ParImpar'
 import Contador from './components/Contador'
+import Plataformas from './components/Plataformas'
 import Inverter, { MegaSena } from './components/Multi'
 
 export default createDrawerNavigator({
+	Plataformas: {
+		screen: Plataformas
+	},
 	Contador: {
 		screen: () => <Contador inicial={50}/>
 	},
